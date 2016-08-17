cd67d46c00741f3b69089f3109b508fd90007d55
Chinonso <chinonso@live.com>
2016-08-17 12:33:16 -0400

    Add files via upload

---
+++ boston_housing/predict.py
@@ -0,0 +1,13 @@
+# Predict on training set and compute F1 score
+from sklearn.metrics import f1_score
+
+def predict_labels(clf, features, target):
+    print "Predicting labels using {}...".format(clf.__class__.__name__)
+    start = time.time()
+    y_pred = clf.predict(features)
+    end = time.time()
+    print "Done!\nPrediction time (secs): {:.3f}".format(end - start)
+    return f1_score(target.values, y_pred, pos_label='yes')
+
+train_f1_score = predict_labels(clf, X_train, y_train)
+print "F1 score for training set: {}".format(train_f1_score)
